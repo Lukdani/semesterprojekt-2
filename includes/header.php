4c39a45f3888c8bd383b5aed6de9e9860fa84c0f
Lukas <stratocaster@live.dk>
2021-06-06 19:36:10 +0200

    optimeret farve på globus-ikon i header

--- includes/header.php
+++ includes/header.php
@@ -1,89 +1,65 @@
 <div class="">
-  <nav class="navbar navbar-expand-lg fixed-top">
-    <a class="navbar-brand" href="index.php">
-      <img src="./images/logo.png" />
-    </a>
-    <button
-      class="navbar-toggler d-lg-none"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item dropdown">
-        <a
-            class="nav-link"
-            href="./det-sker.php"
-          >
-            Det sker
-          </a>
-        </li>
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <a class="navbar-brand" href="index.php">
+            <img src="./images/logo.png" />
+        </a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="./det-sker.php">
+                        Det sker
+                    </a>
+                </li>
 
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link"
-            href="./familier-og-grupper.php"
-          >
-            Familier og grupper
-          </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link"
-            href="./rundvisninger.php"
-          >
-            Rundvisninger
-          </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a
-            class="nav-link dropdown-toggle"
-            href="#"
-            id="navbarDropdown"
-            role="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Om Trelleborg
-          </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="fejl404.html">Trelleborgs historie</a>
-            <a class="dropdown-item" href="fejl404.html">Unesco</a>
-            <a class="dropdown-item" href="fejl404.html">Ny Trelleborg</a>
-          </div>
-        </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="./familier-og-grupper.php">
+                        Familier og grupper
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="./rundvisninger.php">
+                        Rundvisninger
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Om Trelleborg
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="fejl404.html">Trelleborgs historie</a>
+                        <a class="dropdown-item" href="fejl404.html">Unesco</a>
+                        <a class="dropdown-item" href="fejl404.html">Ny Trelleborg</a>
+                    </div>
+                </li>
 
-          <li class="nav-item dropdown">
-              <a
-                      class="nav-link"
-                      href="KontaktOgInfo.php"
-              >
-                  Kontakt og Info
-              </a>
-          </li>
-        <li class="nav-item">
-          <a href="" class="nav-link">
-          <i class="fas fa-globe text-green"></i>
-          EN
-          </a>
-        </li>
-  
-      </ul>
-          <a href="" class="">
-          <button class="btn btn-primary">
-            Booking
-          </button>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="KontaktOgInfo.php">
+                        Kontakt og Info
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link">
+                        <i class="fas fa-globe text-green-light"></i>
+                        EN
+                    </a>
+                </li>
 
-          </a>
+            </ul>
+            <a href="" class="">
+                <button class="btn btn-primary">
+                    Booking
+                </button>
 
-    </div>
-  </nav>
+            </a>
+
+        </div>
+    </nav>
 </div>
 <div class="navbar-spacer">
     &nbsp;
