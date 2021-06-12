4b4926ac10cf6c785427797a2a95c83abe72ddca
Lukas <stratocaster@live.dk>
2021-06-12 22:52:22 +0200

    tilfoejet favicon

--- includes/dependencies.php
+++ includes/dependencies.php
@@ -2,6 +2,7 @@
 <meta name="robots" content="All">
 <meta name="author" content="Udgiver">
 <meta name="copyright" content="Information om copyright">
+<link rel="icon" type="image/png" href="/images/favicon.png" />
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
